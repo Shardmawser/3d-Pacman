c484a1ab1e8baedd45028927346521825a367e4d
Shardmawser <abantolin777@gmail.com>
2023-01-03 07:41:43 +0800

    Changed direction of fill
    It now fills from left to right, top to bottom instead of it being in the center

--- 3d Pacman/Assets/Scripts/GameManager.cs
+++ 3d Pacman/Assets/Scripts/GameManager.cs
@@ -1,34 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] float cols = 50f;
-    [SerializeField] float rows = 50f;
-    [SerializeField] float offset = 2f;
+    public GameObject prefab; // the prefab to instantiate
+    public float width; // width of the area
+    public float height; // height of the area
+    public float spacing; // spacing between the game objects
 
-    [SerializeField] Transform startingPoint;
-
-    [SerializeField] GameObject point;
-
-    GameObject newPoint;
-    Transform newPointPos;
-
-    private void Start()
+    void Start()
     {
-        for(int i = 0; i < rows; i++)
-        {
-            for(int j = 0; j < cols; j++)
-            {
-                newPoint = Instantiate(point, startingPoint.position, Quaternion.identity);
+        // calculate the number of game objects to instantiate in the x and y directions
+        int numX = Mathf.RoundToInt(width / spacing);
+        int numY = Mathf.RoundToInt(height / spacing);
 
-                newPoint.transform.position = new Vector3(newPoint.transform.position.x - i, newPoint.transform.position.y, newPoint.transform.position.z + j);
+        // iterate through the x and y directions and instantiate game objects
+        for (int x = 0; x < numX; x++)
+        {
+            for (int y = 0; y < numY; y++)
+            {
+                // calculate the position of the game object
+                float xPos = x * spacing - width / 2f + spacing / 2f;
+                float yPos = y * spacing - height / 2f + spacing / 2f;
+
+                // create a new game object at the calculated position
+                Vector3 position = new Vector3(xPos, 0f, yPos);
+                GameObject go = Instantiate(prefab, position, Quaternion.identity);
+
+                // set the parent of the game object to this transform
+                go.transform.parent = transform;
             }
-        }        
+        }
     }
 
 }
